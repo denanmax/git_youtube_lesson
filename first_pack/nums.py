3ec5eef70ea7720cab9b25377535ea0edd436158
Denis <deshis93@gmail.com>
2023-04-21 13:01:11 +0200

    new func multiply added print results with all func added

--- first_pack/nums.py
+++ first_pack/nums.py
@@ -6,5 +6,11 @@ text = "new text"
 def diff(a,b):
     return a-b
 
+def multiply(a, b):
+    return a*b
+
 if __name__ == '__main__':
     print(sum(2, 2))
+    print(diff(7, 4))
+    print(multiply(5, 2))
+
